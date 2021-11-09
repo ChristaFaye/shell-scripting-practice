c3eb11b15def4e226b2208749fbd44565c4ffb8f
christafayereyes <christafayereyes@gmail.com>
2021-11-09 13:02:24 -0600

    Added while loop for the menu loop

--- calc.sh
+++ calc.sh
@@ -1,33 +1,37 @@
-# Input numbers
-  echo ""
-  echo -e "Input Numbers..."
-  echo -n "Enter first number: "
-  read NUM1
-  echo -n "Enter second number: "
-  read NUM2
+while true
+do
+  # Input numbers
+    echo ""
+    echo -e "Input Numbers..."
+    echo -n "Enter first number: "
+    read NUM1
+    echo -n "Enter second number: "
+    read NUM2
 
-# Input type of operation
-  echo "Enter number of choice:"
-  echo "1 - Addition"
-  echo "2 - Subtraction"
-  echo "3 - Exit"
-  echo -n "Choice: "
-  read OP
+  # Input type of operation
+    echo "Enter number of choice:"
+    echo "1 - Addition"
+    echo "2 - Subtraction"
+    echo "3 - Exit"
+    echo -n "Choice: "
+    read OP
 
-  case $OP in
-    1)
-      let "SUM= $NUM1 + $NUM2"
-      echo "Result:" $NUM1 "+" $NUM2 "=" $SUM
-      ;;
-    2)
-      let "DIFF= $NUM1 + $NUM2"
-      echo "Result:" $NUM1 "-" $NUM2 "=" $DIFF
-      ;; 
-    3)
-      echo "$Thank you for using my amazing calculator!"
-      break
-      ;;
-    *)
-      echo "$Invalid input. Please try again."
-      ;;
-  esac
+    case $OP in
+      1)
+        let "SUM= $NUM1 + $NUM2"
+        echo "Result:" $NUM1 "+" $NUM2 "=" $SUM
+        ;;
+      2)
+        let "DIFF= $NUM1 + $NUM2"
+        echo "Result:" $NUM1 "-" $NUM2 "=" $DIFF
+        ;; 
+      3)
+        echo "$Thank you for using my amazing calculator!"
+        break
+        ;;
+      *)
+        echo "$Invalid input. Please try again."
+        ;;
+    esac
+
+  done
